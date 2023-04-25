6728a3c4287e36cbff56e8ebaeff2d701e3d897e
Rodrigo Silva <rodrigob_silva@hotmail.com>
2023-04-25 09:59:16 -0400

    Add all seed data

--- seeders/20230425133911-employee.js
+++ seeders/20230425133911-employee.js
@@ -3,13 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Parties', [{
-       numberOfAdults: 3,
-       numberOfChildren: 3,
-     }], {});
+      await queryInterface.bulkInsert('Employees', [{
+        name: 'John Doe',
+        sectionId: 1  
+      }], {});
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('People', null, {});
+    await queryInterface.bulkDelete('Employees', null, {});
   }
 };
